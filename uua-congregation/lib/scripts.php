963e93e20462eb1d23f1c7785bf10a2043e90133
Kevin Vess <kevin@vess.me>
2018-03-09 22:37:24 -0500

    Cleaned up scripts and styles enqueue

--- uua-congregation/lib/scripts.php
+++ uua-congregation/lib/scripts.php
@@ -1,29 +1,26 @@
 <?php
-/**
- * Scripts and stylesheets
- */
+if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Register Theme Scripts and Styles
+ */
 if ( ! function_exists('uuatheme_scripts') ) {
 
-  function uuatheme_scripts() {
-    $assets     = array(
-      'css'       => '/style.css',
-      'js'        => '/assets/js/scripts.js',
-    );
+    function uuatheme_scripts() {
 
-    wp_enqueue_style('styles', get_stylesheet_uri(), false, filemtime( get_template_directory() . '/style.css'), 'all');
-    wp_enqueue_style('fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', null, '4.7.0');
+        // Enqueue Styles.
+        wp_enqueue_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
+        wp_enqueue_style( 'uuatheme-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css') );
 
-    if (is_single() && comments_open() && get_option('thread_comments')) {
-      wp_enqueue_script('comment-reply');
-    }
+        // Enqueue Scripts.
+        wp_enqueue_script( 'uuatheme-script', get_template_directory_uri() . '/assets/js/scripts.js', array( 'jquery' ), false, true );
 
-    // Commented out per http://uuatheme.org/question/v1-1-theme-jquery-is-breaking-other-plugins-heres-a-workaround/
-    // wp_deregister_script( 'jquery' );
-    // wp_register_script( 'jquery', "//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js", array(), '3.2.1' );
+        // Load single scripts only on single pages.
+        if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+            wp_enqueue_script( 'comment-reply' );
+        }
 
-    wp_enqueue_script('uuatheme_js', get_template_directory_uri() . $assets['js'], array('jquery'), null, false);
   }
-  add_action('wp_enqueue_scripts', 'uuatheme_scripts', 100);
+  add_action( 'wp_enqueue_scripts', 'uuatheme_scripts', 100 );
 
 }
